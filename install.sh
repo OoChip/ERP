7a714688d606ad6c8be3165d39a2c9bd7cf9781c
OoChip <64329445+OoChip@users.noreply.github.com>
2022-07-14 21:25:20 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -20,5 +20,5 @@ sudo chmod +x ERP/upnp_ddns/script.sh && sudo mv ERP/upnp_ddns/script.sh /bin &&
 sudo systemctl enable cron
 
 #Portainer.
-docker run -d -p 8000:8000 -p 9443:9443 --name portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:latest
+docker run -d -p 8000:8000 -p 81:9443 --name portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:latest
 docker run -d -p 9001:9001 --name portainer_agent --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v /var/lib/docker/volumes:/var/lib/docker/volumes -v /:/host portainer/agent:2.10.0
